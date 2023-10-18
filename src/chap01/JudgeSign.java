b75787c375125fb299b79ceb65c4b4492203bdee
uu1010 <yoooomi030@gmail.com>
2023-10-18 23:15:37 +0900

    프로그램 분기

--- src/chap01/JudgeSign.java
+++ src/chap01/JudgeSign.java
@@ -10,7 +10,8 @@ public class JudgeSign {
         System.out.println("정수를 입력하세요: ");
         int n = stdIn.nextInt();
 
-        if (n > 0) {
+        //프로그램 흐름을 세가지로 분기
+        if (n > 0) { //실행되는 부분은 세가지 경우 중 하나뿐이며, 두가지가 동시에 실행되거나 실행되지 않는 경우는 없음.
             System.out.println("이 수는 양수입니다");
         } else if (n < 0) {
             System.out.println("이 수는 음수입니다");
